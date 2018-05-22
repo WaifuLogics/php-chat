a17b7850f77e92b205d3bcf839a073082b88f3ab
Duncan Sterken <duncan.sterken123@gmail.com>
2018-05-22 14:49:44 +0200

    fix parsedown attempt 1

--- src/pmill/Chat/parsedown/ParseDownExtension.php
+++ src/pmill/Chat/parsedown/ParseDownExtension.php
@@ -9,18 +9,28 @@ class ParseDownExtension extends Parsedown
     protected function inlineUrl($Excerpt)
     {
         $link = parent::inlineUrl($Excerpt);
+        if(is_null($link))
+            return;
+
         $link['element']['attributes']['target'] = '_blank';
         return $link;
     }
 
     protected function inlineLink($Excerpt) {
         $link = parent::inlineLink($Excerpt);
+
+        if(is_null($link))
+            return;
+
         $link['element']['attributes']['target'] = '_blank';
         return $link;
     }
 
     protected function inlineImage($Excerpt) {
         $image =  parent::inlineImage($Excerpt);
+
+        if(is_null($image))
+            return;
 
         $image['element']['attributes']['style'] = "max-width: 500px;";
 
