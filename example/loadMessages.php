5fc2a13642af1acba058c331e3555b373dbc988a
Duncan Sterken <duncan.sterken123@gmail.com>
2018-05-24 10:56:31 +0200

    Parse received messages

--- example/loadMessages.php
+++ example/loadMessages.php
@@ -1,6 +1,10 @@
 <?php
+
 header('Content-Type: application/json');
 require_once __DIR__ . "/../vendor/autoload.php";
+
+use pmill\Chat\parsedown\ParseDownExtension;
+
 $roomId = isset($_GET['room']) && !empty($_GET['room']) ? ((integer)$_GET['room']) : 0;
 
 $db = \pmill\Chat\Database\DBDriver::getDatabase();
@@ -9,6 +13,18 @@ $smt = $db->prepare("SELECT * FROM chat_room_messages WHERE chat_room_id = :room
 $smt->execute([
     'room' => $roomId
 ]);
+
+$messages = [];
+$parsedown = new ParseDownExtension();
+$parsedown->setSafeMode(true)
+    ->setBreaksEnabled(true);
+
+foreach ($smt->fetchAll(PDO::FETCH_ASSOC) as $message) {
+    $messages[] = [
+        'account_name' => $message['account_name'],
+        'chat_message' => $parsedown->line($message['chat_message'])
+    ];
+}
 
 die(
     json_encode([
