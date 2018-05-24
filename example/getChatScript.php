5df418056a756b88e4b945f54088bc0af339fdfd
Duncan Sterken <duncan.sterken123@gmail.com>
2018-05-24 10:00:30 +0200

    add quotes

--- example/getChatScript.php
+++ example/getChatScript.php
@@ -4,6 +4,6 @@ $roomId = isset($_GET['room']) && !empty($_GET['room']) ? ((integer)$_GET['room'
 $chat = isset($_GET['chat']) && !empty($_GET['chat']) ? $_GET['chat'] : 'ws://protask.duncte123.me:9911';
 
 echo 'let room = ' . $roomId . ';' . PHP_EOL;
-echo 'let chatUrl = ' . $chat . ';' . PHP_EOL;
+echo 'let chatUrl = "' . $chat . '";' . PHP_EOL;
 
 echo file_get_contents(__DIR__ . '/chat.js');
