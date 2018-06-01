0f3e427f346c329c9ea7109e393dbcb622a9bf06
Duncan Sterken <duncan.sterken123@gmail.com>
2018-06-01 13:07:29 +0200

    Take some code from google

--- example/chat.js
+++ example/chat.js
@@ -39,7 +39,7 @@ function displayChatMessage(from, message, timestamp) {
         `;
 
         console.log(timestamp);
-        console.log(new Date(Date.parse(timestamp)));
+        console.log(new Date(+timestamp));
 
         rowNode.appendChild(messageNode);
         node.appendChild(rowNode);
